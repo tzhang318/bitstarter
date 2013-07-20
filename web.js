ad2ced3a1bb1b14047cbcc4a6644cf050e170b1e
Ubuntu <ubuntu@ip-172-31-32-120.us-west-2.compute.internal>
2013-07-20 14:39:54 +0000

    add directory to static jpg files

--- web.js
+++ web.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var fs = require('fs');
-
-var buff = fs.readFileSync("index.html");
-var outputLine = buff.toString('utf8', 0);
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(outputLine);
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+app.use(express.static(__dirname + '/img'));
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
